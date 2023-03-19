1c19a825035e05bd35487c0ac45b1c532c09a726
KarolT <karolteczar@yahoo.com>
2023-03-20 00:13:00 +0100

    Dodaje zmiany do VaRexercise, funkcja breachMcSym do poprawy

--- VaRexercise.py
+++ VaRexercise.py
@@ -4,11 +4,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import estimator
 
-mu = 1.0
-sigma = np.sqrt(10)
 
 def varSym(samplesize=250, nruns=10_000, alfa=0.01, mu=0.0, sigma=1.0):
-    """This function makes nruns simulations to estimate Value at Risk"""
+    """This function makes nruns simulations to estimate Value at Risk.
+    The purpose was to  compare different estimators """
     varTable = []
     for j in range(1, nruns):
         data = np.random.normal(mu, sigma, samplesize)
@@ -19,47 +18,57 @@ def varSym(samplesize=250, nruns=10_000, alfa=0.01, mu=0.0, sigma=1.0):
             "nruns": nruns,
             "confLvl": alfa}
 
+#global mu,sigma = 1.0, np.sqrt(10)
 
 # symulacja = varSym(mu = 1.0, sigma = np.sqrt(10))
 # print(symulacja)
 
-def breachSym(data, days=250):
-    empCount, normCount = 0, 0
-    empirical, normal = [], []
+def breachSym(data, days=250, alfa=0.01):
+    """This function returns number of VaR breaches in a given dataset.
+    days - we estimate VaR on past num of days (by default 250) and
+    check if we have bigger losses than Value at Risk"""
+    empCount, normCount = 0, 0  # Creating counter for Var breaches
+    empirical, normal = [], []  # to store Value at Risk
 
     for i in range(1, days + 1):
-        temp = data[i - 1:days - 1 + i]
+        temp = data[i - 1:days - 1 + i]  # we take last "days" amout of days to estimate VaR
 
-        emp = estimator.var_emp(temp, alfa=0.01)
-        norm = estimator.var_unorm(temp, alfa=0.01)
+        emp = estimator.var_emp(temp, alfa)
+        norm = estimator.var_unorm(temp, alfa)
 
         empirical.append(emp)
         normal.append(norm)
 
-        if (emp < data[days - 1 + i]): empCount = empCount + 1
+        if (emp < data[days - 1 + i]): empCount = empCount + 1  # checking wether losses were greater than Value at Risk
         if (norm < data[days - 1 + i]): normCount = normCount + 1
 
     return {"empBreach": empCount,
-            "normBrach": empCount,
+            "normBreach": empCount,
             "empVar": empirical,
             "normVar": normal,
             "profitLoss": data[-days:]}
 
+mu,sigma = 1.0, np.sqrt(10)
+# data = np.random.normal(mu, sigma, 500)
+# symulacja = breachSym(data)
+# print(symulacja.keys())
+# print(symulacja["empBreach"])
 
-#data = np.random.normal(mu, sigma, 500)
-#symulacja = breachSym(data)
-#print(symulacja.keys())
-#print(symulacja["empBreach"])
+def breachMcSym(mu=0.0, sigma=1.0, nruns=100, alfa=0.01, days=250):
 
-tmp_emp = []
-tmp_norm = []
-for i in range(1, 100):
-    data = np.random.normal(mu, sigma, 500)
-    symulacja = breachSym(data)
-    tmp_emp.append(symulacja["empBreach"])
-    tmp_norm.append(symulacja["normBrach"])
+    for i in range(1, nruns):
+        data = np.random.normal(mu, sigma, 500)
+        tmp_emp, tmp_norm = [], []
+        symulacja = breachSym(data, days, alfa)
+        tmp_emp.append(symulacja["empBreach"])
+        tmp_norm.append(symulacja["normBreach"])
 
-print(f'{statistics.mean(tmp_emp)}\n{statistics.mean(tmp_emp)}')
+    print(f'Avrage number of VaR breaches for\nEmpirical estimator: {statistics.mean(tmp_emp)}\n'
+          f'Unbiased normal estimator: {statistics.mean(tmp_emp)}')
+
+
+#breachMcSym(mu, sigma, nruns=300) #do poprawy, cos nie dziala
+
 
 # empirical = []
 # normal = []
