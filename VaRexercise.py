d62af0086005c63b62d30409c495ce56a66eb7e7
KarolT <karolteczar@yahoo.com>
2023-03-19 19:41:58 +0100

    Including exercise 3 and 4 in VaRexercise.py

--- VaRexercise.py
+++ VaRexercise.py
@@ -1,64 +1,86 @@
 import statistics
+
+import matplotlib.pyplot as plt
 import numpy as np
 import estimator
 
-
-tablica = []
 mu = 1.0
 sigma = np.sqrt(10)
 
-
-def varSym(samplesize = 250, nruns = 10_000, alfa = 0.01, mu = 0.0, sigma = 1.0):
+def varSym(samplesize=250, nruns=10_000, alfa=0.01, mu=0.0, sigma=1.0):
     """This function makes nruns simulations to estimate Value at Risk"""
     varTable = []
-    for i in range(1, nruns):
+    for j in range(1, nruns):
         data = np.random.normal(mu, sigma, samplesize)
         var = estimator.var_unorm(data, alfa)
         varTable.append(var)
-    return {"meanVar" : statistics.mean(varTable),
-            "std_dev" : statistics.stdev(varTable),
-            "nruns"   : nruns,
-            "confLvl" : alfa}
+    return {"meanVar": statistics.mean(varTable),
+            "std_dev": statistics.stdev(varTable),
+            "nruns": nruns,
+            "confLvl": alfa}
 
 
-symulacja = varSym(mu = 1.0, sigma = np.sqrt(10))
-print(symulacja)
+# symulacja = varSym(mu = 1.0, sigma = np.sqrt(10))
+# print(symulacja)
 
-#for i in range(1, 10_000):
-#    s = np.random.normal(mu, sigma, 250)
-#    mu_hat = statistics.mean(s)
-#    sigma_hat = statistics.stdev(s)
-#    _VaR_99 = estimator.var_emp(s)
-#    tablica.append(_VaR_99)
+def breachSym(data, days=250):
+    empCount, normCount = 0, 0
+    empirical, normal = [], []
 
-#print(f'Wartosc oczekiwana:{np.mean(tablica)} ,\nStandard dev: {statistics.stdev(tablica)}')
+    for i in range(1, days + 1):
+        temp = data[i - 1:days - 1 + i]
 
-# tablica2 = []
-# for i in range (1,10_000):
-#    s = np.random.normal(mu, sigma, 250)
-#    temp_mu_hat = statistics.mean(s)
-#    temp_sigma_hat = statistics.stdev(s)
-#    _VaR_99 = -(temp_mu_hat + temp_sigma_hat*norm.ppf(1-0.99, mu, sigma))
-#    tablica2.append(_VaR_99)
+        emp = estimator.var_emp(temp, alfa=0.01)
+        norm = estimator.var_unorm(temp, alfa=0.01)
+
+        empirical.append(emp)
+        normal.append(norm)
+
+        if (emp < data[days - 1 + i]): empCount = empCount + 1
+        if (norm < data[days - 1 + i]): normCount = normCount + 1
+
+    return {"empBreach": empCount,
+            "normBrach": empCount,
+            "empVar": empirical,
+            "normVar": normal,
+            "profitLoss": data[-days:]}
+
+
+#data = np.random.normal(mu, sigma, 500)
+#symulacja = breachSym(data)
+#print(symulacja.keys())
+#print(symulacja["empBreach"])
+
+tmp_emp = []
+tmp_norm = []
+for i in range(1, 100):
+    data = np.random.normal(mu, sigma, 500)
+    symulacja = breachSym(data)
+    tmp_emp.append(symulacja["empBreach"])
+    tmp_norm.append(symulacja["normBrach"])
+
+print(f'{statistics.mean(tmp_emp)}\n{statistics.mean(tmp_emp)}')
+
+# empirical = []
+# normal = []
+# data = np.random.normal(mu, sigma, 500)
 #
-# print(f' Dla estymatora normalnego: \nWart.ocz: {statistics.mean(tablica2)}\nStd.dev: {statistics.stdev(tablica2)}')
-
-# s = np.random.normal(mu, sigma, 500)
-# VaR_99 = norm.ppf(1-0.99, mu_hat, sigma_hat)
-
-# licznik = 0
-# tablica = []
-# pl = []
-# for i in range(0,249):
-#    temp = s[i: 249+i]
-#    mu_hat = statistics.mean(temp)
-#    sigma_hat = statistics.stdev(temp)
-#    _VaR_99 = -norm.ppf(1 - 0.99, mu_hat, sigma_hat)
-#    tablica.append(_VaR_99)
-
-#    if(s[250+i] > _VaR_99): licznik = licznik+1
-#    pl.append(s[250+i] - temp[249+i])
-
-
-# plt.plot(pl)
-# print(f'Wartosc Var: {statistics.mean(tablica)}\nIlosc przekroczen VaRu :{licznik}')
+# empCount, normCount = 0, 0
+# for i in range(1, 251):
+#    temp = data[i - 1:249 + i]  # tablica zawierajaca zmiany cen z 250 dni
+#
+#    emp = estimator.var_emp(temp, alfa=0.01)
+#    norm = estimator.var_unorm(temp, alfa=0.01)
+#
+#    empirical.append(emp)
+#    normal.append(norm)
+#
+#    if (emp < data[250 - 1 + i]): empCount = empCount + 1
+#    if (norm < data[250 - 1 + i]): normCount = normCount + 1
+#
+# print(empCount, normCount)
+# plt.plot([-x for x in empirical])
+# plt.plot([-x for x in normal])
+# plt.plot(tablica[250:501])
+# plt.show()
+#
